41dd19cf02cefe2ff83755fa5cc0a7cbd3754031
Kato Twofold <kato@chunkbyte.com>
2021-05-30 18:19:27 +0300

    Started to actually work on the project

---
+++ src/config/UserConfig.ts
@@ -0,0 +1,67 @@
+// CONSTANTS
+const itemTypes = {
+    NOTE: `note`, // Variables [ readOnly: boolean(false), text: string(""), persistent: boolean(true)]
+    LINK: `link`, // Variables (*:string) [ icon(image link), title, subtitle, link ]
+};
+
+// A list of available widgets that have been integrated, please list the in an array in the 
+const availableWidgets = {
+    // A widget to keep up with the value of crypto currency
+    coinWatch: (coinType : string = "ETH", targetFiat : string = "EUR") => {
+        return { coinType: coinType, targetFiat: targetFiat };
+    },
+};
+
+export = {
+    // The main navigation bar
+    header: {
+        // Enable or disable the whole top-navbar
+        enabled: true,
+        // Enable the search function on the right hand side of the navbar
+        enableSearch: true,
+        // The search engine to use for for the box, 'https://google.com/search', 'https://duckduckgo.com/'
+        searchEngine: 'https://www.google.com/search',
+        // The search param, this usually is 'q' for google and duckduckgo
+        searchParam: `q`,
+        // If the search box should be focused when accessing the page
+        searchEngineFocus: false,
+        // Menu items
+        menuItems: [
+            {
+                // The bootstrap 5 icon of the entry
+                icon: ``,
+                // The text to display of the item
+                text: `Home`,
+                // The link can also be javascript:
+                link: `/`
+            },
+            {
+                icon: ``,
+                text: `About`,
+                link: `https://justkato.me/work`
+            },
+        ],
+    },
+
+    // Widgets to enable in the widgets menu on the top side of the dashboard
+    widget: [
+        availableWidgets.coinWatch(`ETH`, `EUR`),
+        availableWidgets.coinWatch(`BTC`, `EUR`),
+    ],
+
+    // The cards to display in the body of the 
+    items: [
+        {
+            // The type of item to display
+            type: itemTypes.NOTE,
+            
+            // If the text should be editable
+            readOnly: false,
+            // The default text to display if nothing is locally stored
+            text: ``,
+            // Keep the text even after quiting the tab
+            persistent: true,
+        }
+    ]
+
+}
--- src/controllers/Homepage.ts
+++ src/controllers/Homepage.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import UserConfig from "../config/UserConfig";
 
 const homepage = ( req:Request, res:Response, next:NextFunction ) => {
 
@@ -7,19 +8,11 @@ const homepage = ( req:Request, res:Response, next:NextFunction ) => {
     res.setHeader(`Pragma`,        `no-cache`);
     res.setHeader(`Expires`,       `Sat, 26 Jul 1997 05:00:00 GMT`);
 
+    console.log(UserConfig);
+
     // Render the home page
     res.render('home', {
-        // This is a simple variable
-        title: 'Page Title',
-        // A simple list example for the loops
-        listExample: {
-            0: {id: 0, name: `ZERO`},
-            1: {id: 1, name: `First`, disabled: true},
-            2: {id: 2, name: `Second`},
-            3: {id: 3, name: `Third`},
-        },
-        // layout: 'main', // Change this from main to your layout ( from the layouts folder ) if you so wish, it's basically the wrapper of it all
-        time: new Date().toLocaleDateString()
+        config: UserConfig,
     });
 }
 
